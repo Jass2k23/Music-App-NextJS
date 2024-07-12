4d2e4683d898e0c5c1697491513202abccfdd65c
Jaskaran Singh <jass2k23@gmail.com>
2024-07-12 23:02:43 +0530

    add Acerternity UI library | add Navbar | add Data

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
+
+  return (
+    <div
+      className={cn("fixed top-20 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <HoveredLink href="/">All Courses</HoveredLink>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default Navbar;
