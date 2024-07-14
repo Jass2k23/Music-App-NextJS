db9202c4a6a6be17add024bc733b55f6a1a857cc
Jaskaran Singh <jass2k23@gmail.com>
2024-07-15 00:02:49 +0530

    minor changes

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -7,11 +7,11 @@ import testimonialContent from "@/data/testimonials";
 function TestimonialCards() {
   return (
     <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
-      <h2 className="text-3xl font-bold text-center mb-8 z-10">
+      <h2 className="text-3xl sm:text-5xl font-bold text-center mb-8 z-10 font-sans">
         Hear our Harmony: Voices of Success
       </h2>
       <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-6xl">
+        <div className="w-full max-w-6xl font-sans">
           <InfiniteMovingCards
             items={testimonialContent}
             direction="right"
